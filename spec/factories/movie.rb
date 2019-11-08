fd2b052643d2b52a68c9921765013d8a5b44c053
Andrés Torres <elfotografo007@gmail.com>
2019-11-08 08:34:20 -0500

    Fix indentation in Movie Factory

--- spec/factories/movie.rb
+++ spec/factories/movie.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
-    to_create { |instance| instance.save }
+to_create { |instance| instance.save }
     factory :movie do
-      name { Faker::DcComics.title }
-      description { Faker::Movies::StarWars.quote }
-      url { Faker::Internet.url }
-      monday { false }
-      tuesday { false }
-      wednesday { false }
-      thursday { false }
-      friday { false }
-      saturday { false }
-      sunday { false }
+        name { Faker::DcComics.title }
+        description { Faker::Movies::StarWars.quote }
+        url { Faker::Internet.url }
+        monday { false }
+        tuesday { false }
+        wednesday { false }
+        thursday { false }
+        friday { false }
+        saturday { false }
+        sunday { false }
     end
-  end
+end
