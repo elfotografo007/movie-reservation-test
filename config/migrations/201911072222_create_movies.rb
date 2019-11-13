5d3d707e68340d23ff97a167b38742da21146564
Andrés Torres <elfotografo007@gmail.com>
2019-11-12 22:49:04 -0500

    Fix indentation and end line

--- config/migrations/201911072222_create_movies.rb
+++ config/migrations/201911072222_create_movies.rb
@@ -1,17 +1,17 @@
 Sequel.migration do
-    change do
-      create_table(:movies) do
-        primary_key :id
-        String :name, null: false
-        String :description, null: false, text: true
-        String :url, null: false
-        FalseClass :monday, null: false, default: false
-        FalseClass :tuesday, null: false, default: false
-        FalseClass :wednesday, null: false, default: false
-        FalseClass :thursday, null: false, default: false
-        FalseClass :friday, null: false, default: false
-        FalseClass :saturday, null: false, default: false
-        FalseClass :sunday, null: false, default: false
-      end
+  change do
+    create_table(:movies) do
+      primary_key :id
+      String :name, null: false
+      String :description, null: false, text: true
+      String :url, null: false
+      FalseClass :monday, null: false, default: false
+      FalseClass :tuesday, null: false, default: false
+      FalseClass :wednesday, null: false, default: false
+      FalseClass :thursday, null: false, default: false
+      FalseClass :friday, null: false, default: false
+      FalseClass :saturday, null: false, default: false
+      FalseClass :sunday, null: false, default: false
     end
   end
+end
